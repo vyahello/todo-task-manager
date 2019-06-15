a2fe290d50831ab743bbe4434a463023ee301511
Volodymyr Yahello <vyahello@gmail.com>
2019-06-15 14:16:36 +0300

    Add application routes

--- app.py
+++ app.py
@@ -1,38 +1,73 @@
 from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Any
 from flask import Flask, render_template
+
+
+class ApplicationError(Exception):
+    """Raises error occurred in an application."""
+    pass
+
+
+class Route(Enum):
+    """The class represents route element."""
+
+    ROOT: str = "/"
+    HOME: str = "index.html"
+
+    @classmethod
+    def from_str(cls, route: str) -> str:
+        """Returns route from a string."""
+        if route == "root":
+            return str(cls.ROOT)
+        if route == "home":
+            return str(cls.HOME.value)
+        raise ApplicationError(f'Given "{route}" route is invalid!')
+
+    def __str__(self) -> str:
+        """Returns string route value."""
+        return self.value
 
 
 class Application(ABC):
     """The class represents an abstract application."""
 
-    root: str = "/"
-    index: str = "index.html"
-
-    @classmethod
+    @property
     @abstractmethod
-    def web_engine(cls) -> Flask:
+    def engine(self) -> Flask:
         """Returns web engine of an application"""
+        pass
+    
+    @abstractmethod
+    def run(self, host: str, port: str, debug: bool = False, load_dot_env: bool = True, **options: Any) -> None:
+        """Runs an application."""
         pass
 
 
-def _run_todo_task_manager():
+class Todo(Application):
+    """The class represents."""
+    
+    def __init__(self, name: str) -> None:
+        self._engine: Flask = Flask(name)
+
+    @property
+    def engine(self) -> Flask:
+        return self._engine
+    
+    def run(self, host: str, port: str, debug: bool = False, load_dot_env: bool = True, **options: Any) -> None:
+        self._engine.run(host, port, debug, load_dot_env, **options)
+
+
+def _run_todo_task_manager(host: str = "localhost", port: str = "7777") -> None:
     """Runs `to-do` task manager application"""
+    todo: Application = Todo(name=__name__)
 
-    class Todo(Application):
-        """The class represents """
-
-        @classmethod
-        def web_engine(cls) -> Flask:
-            return Flask(__name__)
-
-    engine: Flask = Todo.web_engine()
-
-    @engine.route(Application.root)
+    @todo.engine.route(Route.from_str("root"))
     def index():
         """Returns an index page of an application."""
-        return render_template(Application.index)
+        return render_template(Route.from_str("home"))
 
-    engine.run()
+    todo.run(host, port)
 
 
 if __name__ == "__main__":
